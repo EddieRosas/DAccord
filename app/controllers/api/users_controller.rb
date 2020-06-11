14ce7790ac1358e3ad0d39d5284210d771d459bb
Eddie Rosas <eddie_rosas@outlook.com>
2020-06-10 17:27:04 -0700

    add update to user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,10 +17,28 @@ class Api::UsersController < ApplicationController
         end
     end
 
+   def update
+    unless current_user.id == params[:id].to_i
+      flash.now[:errors] = ['You do not have permissions to edit this user.']
+    else
+      @user = current_user
+      @user.image.purge
+      unless @user.update(edit_params)
+        flash.now[:errors] = @user.errors.full_messages
+      else
+        render :update, status: 200
+      end
+    end
+  end
+
     private
 
     def user_params 
         params.require(:user).permit(:email, :username, :password)
+    end
+
+    def edit_params
+    user_params = params.require(:user).permit(:image)
     end
         
 end
